f893d2e86077a730b33836f4fbaee11ee16c9359
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-09-29 11:51:52 +0600

    Add preprocessor directive example in C++

---
+++ Variable & Data Types/3. Preprocessor Directive.cpp
@@ -0,0 +1,9 @@
+#include <iostream>    //preprocessor directive
+#define PI 3.1416      // This is also a one kind of preprocessor directive
+using namespace std;
+
+int main(){
+    cout<<PI<<endl;
+
+    return 0;
+}
