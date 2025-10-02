3aaa66f6dc465a1d29f88e5a70af128a7f8e694b
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-10-02 11:57:46 +0600

    Delete Variable & Data Types directory

--- Variable & Data Types/3. Preprocessor Directive.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>    //preprocessor directive
-#define PI 3.1416      // This is also a one kind of preprocessor directive
-using namespace std;
-
-int main(){
-    cout<<PI<<endl;
-
-    return 0;
-}
