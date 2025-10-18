4989b81a2f6cd7fe28738c2493ddbb367ceb17bb
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-10-18 18:59:35 +0600

    Create 9.4 Comments.cpp

---
+++ 1. Variable & Data Types/9.4 Comments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Single-line comment: declaring variable
+    int age = 21;  
+
+    /* Multi-line comment:
+       We calculate the age for next year
+       and print it on the screen.
+    */
+    int nextYearAge = age + 1;
+
+    cout << "Current age: " << age << endl;      // display current age
+    cout << "Next year age: " << nextYearAge;    // display next year's age
+
+    return 0; // end of program
+}
