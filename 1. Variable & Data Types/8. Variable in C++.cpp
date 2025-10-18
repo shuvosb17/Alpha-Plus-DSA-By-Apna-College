29b1eef3f1ba9bc81957f5afef97fdd686ad68be
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-10-18 11:40:08 +0600

    Create 8. Variable in C++.cpp

---
+++ 1. Variable & Data Types/8. Variable in C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable declaration and initialization
+    int age = 20;           // integer variable
+    float height = 5.9;     // floating-point variable
+    char grade = 'A';       // character variable
+    string name = "Tanvir"; // string variable
+    bool isStudent = true;  // boolean variable
+
+    // Displaying variable values
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " feet" << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
